ffc608abcf5560bb98a1616ac5186d6fabc9b1b1
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-06-01 14:26:38 +0200

    load level

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var bpmnJs = require('bpmn-js');
-console.log(bpmnJs);
-console.log('what else');
+var BpmnViewer = require('bpmn-js');
+var fs = require('fs');
+
+var xml = fs.readFileSync(__dirname + '/levels/level1.bpmn', 'utf8');
+
+window.addEventListener('load', function() {
+
+  var viewer = new BpmnViewer({ container: document.body });
+  viewer.importXML(xml, function(err) {
+    if (err) {
+      console.log('error rendering', err);
+    } else {
+      console.log('rendered');
+    }
+  });
+});
+
