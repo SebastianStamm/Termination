975f5f84aa1db6d2332dbb047c282e7ce59c51ba
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-05-04 11:16:43 +0200

    add smartphone controls

---
+++ m/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+
+const app = express();
+
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const viewers = [];
+const players = [];
+
+wss.on('connection', function connection(ws) {
+  const location = url.parse(ws.upgradeReq.url, true);
+  // You might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+  ws.on('close', function close() {
+    if(viewers.indexOf(ws) !== -1) {
+      viewers.splice(viewers.indexOf(ws), 1);
+    }
+    if(players.indexOf(ws) !== -1) {
+      sendMsg(ws, 'CLOSE');
+      players.splice(players.indexOf(ws), 1);
+    }
+  });
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    const parts = message.split(' ');
+
+    if(parts[0] === 'INIT' && parts[1] === 'v') {
+      viewers.push(ws);
+    }
+
+    if(parts[0] === 'INIT' && parts[1] === 'c') {
+      players.push(ws);
+    }
+
+    sendMsg(ws, message);
+  });
+});
+
+function sendMsg(from, msg) {
+  viewers.forEach(viewer => {
+    viewer.send(players.indexOf(from) + ':' + msg);
+  });
+}
+
+server.listen(8080, function listening() {
+  console.log('Listening on %d', server.address().port);
+});
