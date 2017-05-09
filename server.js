2190484504d41d26fe6549947c7f394cfb671f9d
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-05-09 10:23:16 +0200

    reduce noise on server

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    // console.log('received: %s', message);
     const parts = message.split(' ');
 
     if(parts[0] === 'INIT' && parts[1] === 'v') {
